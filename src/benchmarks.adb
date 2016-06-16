e83173ca114e680070ae5d4c4a0f30511fd1f5f5
Hadrien G <knights_of_ni@gmx.com>
2016-06-16 16:29:30 +0200

    Various benchmark tweaks:    - Clarified null task benchmark    - Removed canceled wait benchmark (unfair to parallel launches)    - Used more busy waiting to make benchmark more representative    - Improved usable range of overhead measurement type    - General display improvements

--- src/benchmarks.adb
+++ src/benchmarks.adb
@@ -3,6 +3,7 @@ with Asynchronous.Executors.Interfaces;
 with Asynchronous.Executors.Objects;
 with Asynchronous.Events.Clients;
 with Asynchronous.Events.Composition.Shortcuts;
+with Asynchronous.Events.Contracts;
 with Asynchronous.Events.Interfaces;
 with Asynchronous.Events.Servers;
 with Asynchronous.Tasks.Trivial;
@@ -35,7 +36,8 @@ package body Benchmarks is
          Start_Time, Event_Init_Time, Schedule_Time, When_All_Time, Wait_Over_Time, End_Time : Ada.Calendar.Time;
          Parallel_Duration, Sequential_Duration, Direct_Run_Duration : Duration;
 
-         procedure Run_Serial_Tests (On : in out Asynchronous.Executors.Objects.Executor) is
+         procedure Run_Serial_Tests (On : in out Asynchronous.Executors.Objects.Executor;
+                                     Executor_Kind : String) is
          begin
             -- Run the serial tests
             Start_Time := Ada.Calendar.Clock;
@@ -48,9 +50,9 @@ package body Benchmarks is
 
             -- Analyze sequential results
             Sequential_Duration := End_Time - Start_Time;
-            Ada.Text_IO.Put_Line ("Serial run on parallel executor took " & Duration'Image (Sequential_Duration) &
-                                    " s (" & Duration'Image (Sequential_Duration / Parallel_Duration) &
-                                    "x slower than parallel):");
+            Ada.Text_IO.Put_Line ("Serial run on " & Executor_Kind & " executor took " &
+                                    Duration'Image (Sequential_Duration) & " s (" &
+                                    Duration'Image (Sequential_Duration / Parallel_Duration) & "x parallel run):");
             Ada.Text_IO.Put_Line ("   - Scheduling took " & Duration'Image (Schedule_Time - Start_Time) & " s.");
             Ada.Text_IO.Put_Line ("   - Synchronization took " & Duration'Image (End_Time - Schedule_Time) & " s.");
          end Run_Serial_Tests;
@@ -83,23 +85,49 @@ package body Benchmarks is
          Ada.Text_IO.Put_Line ("   - Event liberation took " & Duration'Image (End_Time - Wait_Over_Time) & " s.");
 
          -- Serial version
-         Run_Serial_Tests (Parallel_Executor);
-         Run_Serial_Tests (Serial_Executor);
+         Run_Serial_Tests (Parallel_Executor, "parallel");
+         Run_Serial_Tests (Serial_Executor, "serial");
 
          -- Direct run (no executor)
          Start_Time := Ada.Calendar.Clock;
+         Task_Loop :
          for I in 1 .. How_Many loop
             declare
                Task_Copy : Asynchronous.Executors.Interfaces.Any_Async_Task := What;
             begin
-               declare
-                  use type Async_Tasks.Return_Status;
-                  Result : constant Async_Tasks.Return_Value := Task_Copy.Run;
-               begin
-                  exit when Async_Tasks.Status (Result) /= Async_Tasks.Yielding;
-               end;
+               Direct_Execution_Loop :
+               loop
+                  declare
+                     use all type Async_Tasks.Return_Status;
+                     Result : constant Async_Tasks.Return_Value := Task_Copy.Run (Was_Canceled => False);
+                  begin
+                     case Async_Tasks.Status (Result) is
+                        when Finished =>
+                           exit Direct_Execution_Loop;
+                        when Yielding =>
+                           null;
+                        when Canceled =>
+                           exit Task_Loop;
+                        when Waiting =>
+                           declare
+                              use all type Asynchronous.Events.Interfaces.Event_Status;
+                              E : constant Asynchronous.Events.Contracts.Valid_Event_Client :=
+                                Asynchronous.Events.Composition.Shortcuts.When_All (Async_Tasks.Wait_List (Result));
+                           begin
+                              case E.Status is
+                                 when Pending =>
+                                    raise Constraint_Error with "Direct runs cannot handle truly blocking tasks";
+                                 when Done =>
+                                    null;
+                                 when Canceled | Error =>
+                                    exit Task_Loop;
+                              end case;
+                           end;
+                     end case;
+                  end;
+               end loop Direct_Execution_Loop;
             end;
-         end loop;
+         end loop Task_Loop;
          Direct_Run_Duration := Ada.Calendar.Clock - Start_Time;
          Ada.Text_IO.Put_Line ("Direct run took " & Duration'Image (Direct_Run_Duration) & " s");
 
@@ -108,24 +136,24 @@ package body Benchmarks is
             Total_Scheduling_Duration : constant Duration := Sequential_Duration - Direct_Run_Duration;
             Scheduling_Overhead : constant Float :=
               Float (Total_Scheduling_Duration) / (Float (How_Many * Internal_Iterations));
-            type Rounded_Overhead is delta 0.01 range 0.0 .. 100.0;
-            Overhead_In_Microseconds : constant Rounded_Overhead := Rounded_Overhead (Scheduling_Overhead * 10.0**6);
+            type Microsecs is delta 0.001 range -1000.0 .. 1000.0;
+            Rounded_Overhead : constant Microsecs := Microsecs (Scheduling_Overhead * 10.0**6);
          begin
             Ada.Text_IO.Put_Line ("The overhead of " & Feature_Name & " is about " &
-                                    Rounded_Overhead'Image (Overhead_In_Microseconds) & " microseconds");
+                                    Microsecs'Image (Rounded_Overhead) & " microseconds");
          end;
          Ada.Text_IO.New_Line;
 
       end Benchmark_Task;
 
-      procedure Benchmark_Startup is
+      procedure Benchmark_Null_Task is
          My_Task : Async_Tasks.Trivial.Null_Task;
       begin
          Benchmark_Task (What         => My_Task,
                          How_Many     => 150_000,
-                         Title        => "startup",
+                         Title        => "null task",
                          Feature_Name => "running the null task");
-      end Benchmark_Startup;
+      end Benchmark_Null_Task;
 
       procedure Benchmark_Yielding is
          Yielding_Iterations : constant := 50_000;
@@ -139,23 +167,26 @@ package body Benchmarks is
                          Internal_Iterations => Yielding_Iterations);
       end Benchmark_Yielding;
 
+      Busy_Waiting_Nanoseconds : constant := 100_000;
+
+      procedure Benchmark_Waiting is
+         My_Task : Async_Tasks.Trivial.Waiting_Task (Busy_Waiting_Nanoseconds);
+      begin
+
+         Benchmark_Task (What                => My_Task,
+                         How_Many            => 50_000,
+                         Title               => "busy-waiting",
+                         Feature_Name        => "running a busy-waiting task");
+      end Benchmark_Waiting;
+
       procedure Benchmark_Wait_Ready is
-         My_Task : Async_Tasks.Trivial.Ready_Wait_Task;
+         My_Task : Async_Tasks.Trivial.Ready_Wait_Task (Busy_Waiting_Nanoseconds);
       begin
          Benchmark_Task (What         => My_Task,
-                         How_Many     => 150_000,
-                         Title        => "nonblocking wait",
+                         How_Many     => 50_000,
+                         Title        => "ready wait",
                          Feature_Name => "waiting for a ready event");
       end Benchmark_Wait_Ready;
-
-      procedure Benchmark_Wait_Canceled is
-         My_Task : Async_Tasks.Trivial.Canceled_Wait_Task;
-      begin
-         Benchmark_Task (What         => My_Task,
-                         How_Many     => 150_000,
-                         Title        => "canceled wait",
-                         Feature_Name => "waiting for a canceled event");
-      end Benchmark_Wait_Canceled;
 
       procedure Benchmark_Wait_Custom is
 
@@ -163,12 +194,19 @@ package body Benchmarks is
          Event_Server_P : constant Asynchronous.Events.Servers.Server := Asynchronous.Events.Servers.Make_Event;
          Event_Client_P : constant Event_Client := Event_Server_P.Make_Client;
          Event_Client_S : constant Event_Client := Event_Server_S.Make_Client;
-         Consumer_Task_P : constant Async_Tasks.Trivial.Custom_Wait_Task := (Target => Event_Client_P);
-         Consumer_Task_S : constant Async_Tasks.Trivial.Custom_Wait_Task := (Target => Event_Client_S);
-         Producer_Task_P : constant Async_Tasks.Trivial.Event_Cancelation_Task := (Target => Event_Client_P);
-         Producer_Task_S : constant Async_Tasks.Trivial.Event_Cancelation_Task := (Target => Event_Client_S);
 
-         Consumer_Count : constant := 100_000;
+         subtype Custom_Wait_Task is Async_Tasks.Trivial.Custom_Wait_Task;
+         subtype Event_Cancelation_Task is Async_Tasks.Trivial.Event_Cancelation_Task;
+         Consumer_Task_P : constant Custom_Wait_Task := (Waiting_Nanoseconds => Busy_Waiting_Nanoseconds,
+                                                        Target => Event_Client_P);
+         Consumer_Task_S : constant Custom_Wait_Task := (Waiting_Nanoseconds => Busy_Waiting_Nanoseconds,
+                                                         Target => Event_Client_S);
+         Producer_Task_P : constant Event_Cancelation_Task := (Waiting_Nanoseconds => Busy_Waiting_Nanoseconds,
+                                                               Target => Event_Client_P);
+         Producer_Task_S : constant Event_Cancelation_Task := (Waiting_Nanoseconds => Busy_Waiting_Nanoseconds,
+                                                               Target => Event_Client_S);
+
+         Consumer_Count : constant := 50_000;
 
          Test_Event : Event_Client;
 
@@ -191,7 +229,7 @@ package body Benchmarks is
             Sequential_Duration := End_Time - Start_Time;
             Ada.Text_IO.Put_Line ("Serial run on parallel executor took " & Duration'Image (Sequential_Duration) &
                                     " s (" & Duration'Image (Sequential_Duration / Parallel_Duration) &
-                                    "x slower than parallel):");
+                                    "x parallel run):");
             Ada.Text_IO.Put_Line ("   - Scheduling took " & Duration'Image (Schedule_Time - Start_Time) & " s.");
             Ada.Text_IO.Put_Line ("   - Synchronization took " & Duration'Image (End_Time - Schedule_Time) & " s.");
          end Run_Serial_Tests;
@@ -233,7 +271,7 @@ package body Benchmarks is
          -- Estimate scheduling overhead
          declare
             Scheduling_Overhead : constant Float := Float (Sequential_Duration) / Float (Consumer_Count + 1);
-            type Rounded_Overhead is delta 0.01 range 0.0 .. 100.0;
+            type Rounded_Overhead is delta 0.001 range -1000.0 .. 1000.0;
             Overhead_In_Microseconds : constant Rounded_Overhead := Rounded_Overhead (Scheduling_Overhead * 10.0**6);
          begin
             Ada.Text_IO.Put_Line ("The overhead of custom waits is about " &
@@ -244,10 +282,10 @@ package body Benchmarks is
       end Benchmark_Wait_Custom;
 
    begin
-      Benchmark_Startup;
+      Benchmark_Null_Task;
       Benchmark_Yielding;
+      Benchmark_Waiting;
       Benchmark_Wait_Ready;
-      Benchmark_Wait_Canceled;
       Benchmark_Wait_Custom;
    end Run_Benchmarks;
 
